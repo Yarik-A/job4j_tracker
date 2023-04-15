e4ebdae5b25b545d9122fd6f7b9c524db1d2b2b5
Yaroslav A <yaroslaav@yandex.ru>
2023-04-15 23:03:14 +0300

    2.1. Тест на ValidateInput [#311887]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -9,17 +9,36 @@ public class ValidateInputTest {
     public void whenInvalidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"one", "1", "2", "3", "-1"}
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo("Please enter valid data" + ln);
+    }
+
+    @Test
+    public void whenThreeValidInput() {
+        Output out  = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "2", "3"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int[] selected = {
                 input.askInt("Enter menu:"),
                 input.askInt("Enter menu:"),
-                input.askInt("Enter menu:"),
                 input.askInt("Enter menu:")
         };
-        assertThat(selected).isEqualTo(new int[] {1, 2, 3, -1});
-        String ln = System.lineSeparator();
-        assertThat(out.toString()).isEqualTo("Please enter valid data" + ln);
+        assertThat(selected).isEqualTo(new int[] {1, 2, 3});
+    }
+
+    @Test
+    public void whenNegativeInput() {
+        Output out  = new StubOutput();
+        Input in = new StubInput(new String[] {"-1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(-1);
     }
 }
